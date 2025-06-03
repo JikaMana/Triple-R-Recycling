b1f85c26948562f260f99de8b97f98f94711a041
Jika <jiksboy2000@gmail.com>
2025-06-03 09:24:18 +0100

    update service to fit Triple R operations

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -123,7 +123,7 @@ export default function ServicesPage() {
             How It Works
           </h2>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-12">
             {steps.map(({ number, title, description }) => (
               <div
                 key={number}
